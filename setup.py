363319bc725a30f4c67971595294c3cbbdbee8da
Carlos-Descalzi <carlos.descalzi@gmail.com>
2020-10-31 18:46:55 -0300

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="podm",
-    version="0.0.9",
+    version="0.0.10",
     author="Carlos Descalzi",
     author_email="carlos.descalzi@gmail.com",
     description="A Object-JSON Document mapper",
